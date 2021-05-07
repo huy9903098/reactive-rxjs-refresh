1172e12a51c43cd444445eee01196bb388538697
Huy <skyzerzlol@gmail.com>
2021-05-08 00:30:25 +0300

    add auth login logout

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+import { AuthStore } from '../service/auth.store';
 
 @Component({
-  selector: 'app-login',
+  selector: 'login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private auth: AuthStore
+  ) {
+    this.form = fb.group({
+      email: ['huy@huy.io', [Validators.required]],
+      password: ['huy', [Validators.required]],
+    });
   }
 
+  ngOnInit() {}
+
+  login() {
+    const val = this.form.value;
+
+    this.auth.login(val.email, val.password).subscribe(
+      () => {
+        this.router.navigateByUrl('/courses');
+      },
+      (err) => {
+        alert('Login failed!');
+      }
+    );
+  }
 }
